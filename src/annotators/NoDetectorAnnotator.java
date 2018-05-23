e770f25900b2a27b13e12391542d61eddbb1832d
Angel <angelsoler91@gmail.com>
2018-05-23 21:57:04 +0200

    Cambios sin comprobar

--- src/annotators/NoDetectorAnnotator.java
+++ src/annotators/NoDetectorAnnotator.java
@@ -1,28 +1,24 @@
 package annotators;
-import java.text.BreakIterator;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
-import java.util.StringTokenizer;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.io.Charsets;
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.FSIterator;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.cas.FSArray;
 import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceAccessException;
 import org.apache.uima.resource.ResourceInitializationException;
 
-import auxiliar.OneSentenceText;
 import auxiliar.Oracion;
 import defecto.NoDetector;
 import es.upm.ctb.midas.clikes.tokenization.Sentence;
-import es.upm.ctb.midas.clikes.tokenization.Token;
 
 
 /**
@@ -32,32 +28,16 @@ import es.upm.ctb.midas.clikes.tokenization.Token;
  */
 public class NoDetectorAnnotator extends JCasAnnotator_ImplBase {
 
-	//private Pattern noPattern = Pattern.compile("\\b(No|no).\\p{Punct}\\b");
-	//private Pattern noPattern = Pattern.compile("(No|no|ni|Tampoco|tampoco|Nunca|nunca|Sin|sin|Ning�n|ning�n)[\\s][a-zA-Z\\s]*[^\\p{Punct}]");
-
-	//Fijarse en esta direccion
-	//http://sujitpal.blogspot.com.es/2011/06/uima-analysis-engine-for-keyword.html
-
-	//private Set<Pattern> patternSet;
-
-	//Map
 	private StringMapResource_impl mMap;
-	//private Map<String,String> mapAux;
+	private HashMap<Integer,NoDetector> mapAux;
 	private List<String> listaPalabras;
 	static String[] SENTENCE;
 	private List<Oracion> oraciones;
-	//private List<Integer> longitudOraciones; //Longitud acumulada
-
-	//ParsePosition pp = new ParsePosition(0);
-
-	// ****************************************
-	// * Static vars holding break iterators
-	// ****************************************
-	static final BreakIterator sentenceBreak = BreakIterator.getSentenceInstance(Locale.US);
 
 	/**
 	 * Método en el que se inicializan:
 	 * - La lista de palabras local a partir de la lectura del archivo Dictionary.txt
+	 * - El hashmap que utiliza para evitar que se anote más de una anotacion que comience en la misma posicion
 	 * @param aContext Contexto UIMA @see {@link UimaContext}
 	 * @throws ResourceInitializationException si no se puede inicializar el "Dictionary"
 	 */
@@ -65,13 +45,9 @@ public class NoDetectorAnnotator extends JCasAnnotator_ImplBase {
 	public void initialize(UimaContext aContext) throws ResourceInitializationException {
 		super.initialize(aContext);
 		try {
-			/*SharedSetResource res = (SharedSetResource)aContext.getResourceObject("");
-			patternSet = new HashSet<Pattern>();
-			for(String patternString : res.getConfig()) {
-				patternSet.add(Pattern.compile(patternString));
-			}*/
+
 			mMap = (StringMapResource_impl)getContext().getResourceObject("Dictionary");
-			//mapAux = mMap.getMap();
+			mapAux = new HashMap<Integer,NoDetector>();
 			listaPalabras = mMap.getLista();
 			oraciones = new ArrayList<Oracion>();
 
@@ -82,9 +58,6 @@ public class NoDetectorAnnotator extends JCasAnnotator_ImplBase {
 
 	@Override
 	public void process(JCas jCas) throws AnalysisEngineProcessException {
-
-		/*byte[] docText = null;
-		docText = jCas.getDocumentText().getBytes();*/
 
 		FSIterator<Annotation> iter = jCas.getAnnotationIndex(Sentence.type).iterator();
 
@@ -105,7 +78,7 @@ public class NoDetectorAnnotator extends JCasAnnotator_ImplBase {
 				System.out.println("LA SENTENCE ES -> " + textoDeSentence);
 				System.out.println("LA SENTENCE COMIENZA EN -> " + s.getBegin());
 				System.out.println("LA SENTENCE TERMINA EN -> " + s.getEnd());
-				
+
 				for(String sAux : listaPalabras) {
 					System.out.println("Evaluando si contiene " + sAux);
 					List<Integer> contenida = isContained(textoDeSentence, sAux);
@@ -123,25 +96,51 @@ public class NoDetectorAnnotator extends JCasAnnotator_ImplBase {
 							inicio = contenida.get(aux) + s.getBegin();
 							fin = inicio + sAux.length();
 
-							NoDetector annotation = new NoDetector(jCas);
-							annotation.setBegin(inicio);
-							annotation.setEnd(fin);
-							annotation.addToIndexes();
+							//Antes de crear la anotacion miro a ver si tengo más anotaciones que comienzan en la misma posicion y que sea 
+							//una anotacion cuyo texto este contenido en la "supuesta" anotacion que quiero añadir
+							//Ej: Si existiera la anotacion Sin sintomas de, no debería de anotar Sin pues esta contenida en una mayor y comienza en la misma posicion
+							NoDetector hashNoDetector = mapAux.get(inicio);
+							if(hashNoDetector!=null) {  
+								//En el caso en el que haya una anotacion, siempre va a ser la que tiene mas longitud
+								//Debo de comparar el caracter en donde termina cada una
+								//Solo voy a sustituir la que tengo en mi hash si el nuevo fin que he encontrado es > que lo que tengo almacenado
+								if(fin > hashNoDetector.getEnd()) {
+									NoDetector annotation = new NoDetector(jCas);
+									annotation.setBegin(inicio);
+									annotation.setEnd(fin);
 
-							System.out.println("Anotado desde " + inicio + " hasta " + fin );
-							
+									mapAux.replace(inicio,annotation);
+								}								
+							}else { //Caso en el que no haya ninguna anotacion previamente guardada
+
+								NoDetector annotation = new NoDetector(jCas);
+								annotation.setBegin(inicio);
+								annotation.setEnd(fin);
+
+								//Ahora no voy a añadirlo a los indexes
+								//annotation.addToIndexes();
+
+								//Guardo la anotacion
+								mapAux.put(inicio,annotation);
+							}
 							aux++;
-						}
-					}
-				}
+						}//Fin while(aux<tam)
+					}//Fin if tam > 0
+				}//Fin for listaPalabras
 				i++;
+			}//Fin i < listaSentences.size()
+			//Aqui debo recorrer las anotaciones y añadirlas a los indices
+			Set<Integer> claves = mapAux.keySet();
+			Iterator<Integer> itClaves = claves.iterator();
+			while(itClaves.hasNext()) {
+				NoDetector nDAux = mapAux.get(itClaves.next());
+				nDAux.addToIndexes();
+				itClaves.next();
 			}
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
-
 
 	@Override
 	public void collectionProcessComplete() throws AnalysisEngineProcessException {
@@ -156,7 +155,7 @@ public class NoDetectorAnnotator extends JCasAnnotator_ImplBase {
 	/**
 	 * @param source
 	 * @param subItem
-	 * @return
+	 * @return List<Integer> que indica la posicion en donde comienza cada palabra
 	 */
 	private static List<Integer> isContained(String source, String subItem) {
 		String pattern = "\\b" + subItem + "\\b";
